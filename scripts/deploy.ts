724708d16dcf4eab239f534277486e9f43d2f4ba
Alfa <59610398+Alfa11917676@users.noreply.github.com>
2023-09-28 16:52:29 +0530

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,46 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const [owner] = await ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+    const USDT = await ethers.getContractFactory("USDT");
+    const usdt = await USDT.deploy()
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    let mint = await usdt.mint(owner.address, ethers.utils.parseUnits('1000000','18'));
+    await mint.wait();
+    console.log("USDT Minted");
 
-  await lock.deployed();
+    const TokenM = await ethers.getContractFactory("TokenM");
+    const token = await TokenM.attach("0x86c6Dc9Dec41C285597ADbb606B32d870702F3E4")
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    const usdt = await USDT.attach(await token.usdt());
+
+    await usdt.mint("0x5Ac41287e2a5f040844Ea74563657CfE05453Aa9", ethers.utils.parseUnits('1000000','18'));
+
+    console.log("Token:", await token.usdt());
+
+    const tokenM = await TokenM.deploy(usdt.address)
+    await tokenM.deployed();
+    console.log("TokenM deployed to:", tokenM.address);
+
+    //
+    //
+      let tx = await usdt.approve(tokenM.address, await usdt.balanceOf(owner.address));
+      await tx.wait();
+      console.log("Approved");
+    //
+    let kyc = await token.kyc("0x2141fc90F4d8114e8778447d7c19b5992F6A0611");
+    await kyc.wait();
+    console.log("KYC", kyc);
+    //
+      kyc = await tokenM.kyc("0x5Ac41287e2a5f040844Ea74563657CfE05453Aa9");
+      await kyc.wait();
+      console.log("KYC", kyc);
+
+      mint = await tokenM.mintUsingUSDT(owner.address, ethers.utils.parseUnits('100','18'));
+      await mint.wait();
+      console.log("Minted");
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
